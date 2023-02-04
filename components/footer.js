3eb2a75c7d61b099b1aefd0936b07b1a1c47adc4
Ashiqur Rahman <web3.0.ashiq@gmail.com>
2023-02-04 14:19:49 +0530

    footer design add

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,30 @@
+import Newslatter from "./_child/newslatter"
+import Link from "next/link"
+import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+
+
 export default function Footer() {
+  const bg = {
+    backgroundImage : "url('/images/footer.png')",
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: "bottom left"
+  }
   return (
-    <div className="text-3xl">footer</div>
+     <footer className="bg-gray-50" style={bg}>
+      <Newslatter></Newslatter>
+      <div className="container mx-auto flex justify-center py-12">
+          <div className="py-5">
+              <div className="flex gap-6 justify-center">
+                    <Link href={"/"}><ImFacebook color="#888888" /></Link>
+                    <Link href={"/"}><ImTwitter color="#888888" /></Link>                    
+                    <Link href={"/"}><ImYoutube color="#888888" /></Link>
+              </div>
+
+              <p className="py-5 text-gray-400">Copyright ©2022 All rights reserved | This template is made with  by Daily Tuition</p>
+              <p className="text-gray-400 text-center">Terms & Condition</p>
+          </div>
+      </div>
+
+    </footer>
   )
 }
